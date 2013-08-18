3a850ff20fc993c743422539304e003683a5e95f
Oliver Maskery <omaskery@googlemail.com>
2013-08-18 23:07:20 +0100

    test passed

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <functional>
 #include <algorithm>
+#include <iostream>
+
+class MechBrain
+{
+public:
+	inline MechBrain(int _magic)
+		: mMagic(_magic) {
+	}
+	inline int magic() const { return mMagic; }
+	
+private:
+	int mMagic;
+};
 
 typedef double Score;
-//define mechbrain
 typedef std::pair<Score,MechBrain> ScoredMech; 
 typedef std::function<ScoredMech(MechBrain)> Rater;
 typedef std::vector<MechBrain> Mem_Brains;
@@ -15,7 +27,7 @@ Scored_Mem_Brains GateProtocol(const Mem_Brains &_brains, Rater _rater)
 	
 	for(auto &brain : _brains)
 	{
-		ScoredMech scored(_rater(brain), brain);
+		ScoredMech scored = _rater(brain);
 		result.push_back(scored);
 	}
 	
@@ -29,4 +41,23 @@ Scored_Mem_Brains GateProtocol(const Mem_Brains &_brains, Rater _rater)
 	return result;
 }
 
+int main(int _argc, char *_argv[])
+{
+	Mem_Brains brains = { MechBrain(4), MechBrain(-10), MechBrain(94) };
+	
+	auto scored = GateProtocol(brains,
+		[](MechBrain _brain) -> ScoredMech
+		{
+			ScoredMech result(_brain.magic(), _brain);
+			return result;
+		}
+	);
+	
+	for(auto s : scored)
+	{
+		std::cout << s.first << std::endl;
+	}
+	
+	return 0;
+}
 
